16162a3548e217a8b2911db7c46e90e5cd0029e5
SidM1811 <92666471+SidM1811@users.noreply.github.com>
2022-02-12 13:08:49 +0000

    Update bubble.ml

--- bubble.ml
+++ bubble.ml
@@ -4,6 +4,13 @@ let rec bubble xs=
     |[x]->xs
     |x::y::ys->if x>y then y::(bubble (x::ys))
                 else x::(bubble (y::ys))
+             | Lorry
+let rec bubble_tr xs fr=
+    match xs with
+    []->List.rev fr
+    |[x]->List.rev (x::fr)
+    |x::y::ys->if x>y then bubble_tr (x::ys) (y::fr)
+                else bubble_tr (y::ys) (x::fr)
 let rec bubbleSort l=
     match l with
     []->l
